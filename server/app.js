f390581a08c12d9b24e7d938fba596eb30ff91f0
Rich <RichGThaGenius@macbook-pro.mynetworksettings.com>
2024-06-11 00:00:56 -0400

    working sign in login and sign in client side

--- server/app.js
+++ server/app.js
@@ -4,6 +4,13 @@ const app = express();
 const cors = require("cors");
 const PORT = process.env.PORT;
 
+//middleware
+const validateSession = require("./src/middleware/validate-session");
+//controllers
+const stylistController = require("./src/controllers/stylists.controller");
+const clientController = require("./src/controllers/clients.controller");
+
+// express and cors dependencies
 app.use(cors());
 app.use(express.json());
 
@@ -16,6 +23,13 @@ mongoose.connect(`${MONGO}/Our-cool-App`);
 db.once("open", () => {
   console.log(`Connected: ${MONGO}`);
 });
+
+
+
+//routes
+app.use("/stylists", stylistController, validateSession);
+
+app.use("/clients", clientController);
 
 app.listen(PORT, () => {
   console.log(`Server is on Port: ${PORT}`);
---
+++ server/src/controllers/clients.controller.js
@@ -0,0 +1,41 @@
+//my require section
+
+const router = require("express").Router();
+const Client = require("../models/clients.models");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const SECRET = process.env.JWT;
+
+// error handling function
+const errorResponse = (res, error) => {
+  return res.status(500).json({
+    Error: error.message,
+  });
+};
+
+router.post("/signup", async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    const client = new Client({
+      email,
+    });
+
+    const newClient = await client.save();
+
+    const token = jwt.sign(
+      { id: newClient._id }, // payload
+      SECRET, //message
+      { expiresIn: "1 day" } // options
+    );
+    res.status(200).json({
+      client: newClient,
+      message: `Welcome! we are glad to have you on our platform`, // message for the new user
+      token,
+    });
+  } catch (error) {
+    errorResponse(res, error);
+  }
+});
+
+module.exports = router;
