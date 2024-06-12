cc9b6b47b1e65d6549e0c450e9bf64bd66510fce
Rich <RichGThaGenius@macbook-pro.mynetworksettings.com>
2024-06-12 19:34:20 -0400

    fixed code bug

--- server/src/models/appointments.model.js
+++ server/src/models/appointments.model.js
@@ -1,24 +1,24 @@
 const mongoose = require("mongoose");
 
 const appointmentSchema = new mongoose.Schema({
-      date: {
-            type: String,
-            required: true,
-      },
-      time: {
-            type: String,
-            required: true,
-      },
-      client: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Client",
-            required: true,
-      },
-      stylist: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Stylist",
-            required: true,
-      }
-});   
+  date: {
+    type: String,
+    required: true,
+  },
+  time: {
+    type: String,
+    required: true,
+  },
+  client: {
+    type: String,
+    ref: "Client",
+    required: true,
+  },
+  stylist: {
+    type: String,
+    ref: "Stylist",
+    required: true,
+  },
+});
 
-module.exports = mongoose.model("Appointment", appointmentSchema)
+module.exports = mongoose.model("Appointment", appointmentSchema);
