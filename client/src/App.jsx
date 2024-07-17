1a450a32f4463d18ee7d090c9fbb6bd116e37280
Richard Gore <richngore18@proton.me>
2024-07-16 22:08:27 -0400

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,13 @@ import React from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Auth from "./Components/Auth/Auth";
-import Login from "./Components/Auth/login/Login";
 import { useEffect, useState } from "react";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Auth />}></Route>
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Auth />}></Route>
       </Routes>
     </div>
   );
