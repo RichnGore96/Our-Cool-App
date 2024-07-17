f9b84654657068ff00fb386c0ac8e862d02d676e
Archana Emayavaran <svarchanae@gmail.com>
2024-07-17 17:04:12 -0400

    test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,12 @@
-import React from "react";
-import "./App.css";
+
+import './App.css';
 
 function App() {
-
   return (
     <div className="App">
-     
+      
     </div>
   );
- 
 }
 
 export default App;
