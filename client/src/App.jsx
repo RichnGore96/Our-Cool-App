3d07e59e1fb266b6c735897433a3762d0447e469
Richard Gore <richngore18@proton.me>
2024-07-21 22:46:06 -0400

    fixed routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,20 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/LandingPage/headers/headers";
+import { Routes, Route } from "react-router-dom";
+/* import Header from "./components/LandingPage/headers/headers";
 import HeroSection from "./components/LandingPage/heroSection/heroSection";
 import Services from "./components/LandingPage/services/services";
 import Stylists from "./components/LandingPage/stylist/stylist";
 import Location from "./components/LandingPage/location/location";
-import Footer from "./components/LandingPage/footer/footer";
+import Footer from "./components/LandingPage/footer/footer"; */
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <HeroSection />
-      <Services />
-      <Stylists />
-      <Location />
-      <Footer />
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </>
   );
 }
 
