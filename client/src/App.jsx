b591116eb94217b2ab9a421a0ef02c939ee95d95
Archana Emayavaran <svarchanae@gmail.com>
2024-07-21 22:25:24 -0400

    revert

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,21 @@
 
 
-
-
 import React from 'react';
 import './App.css';
-import { Header, HeroSection, Services, Stylists, Location, Footer } from './components/landing/landing.jsx';
+import Header from './components/landing/headers';
+import HeroSection from './components/landing/heroSection';
+import Services from './components/landing/services';
+import Stylists from './components/landing/stylist';
+import Location from './components/landing/location';
+import Footer from './components/landing/footer';
+import { useNavigate } from "react-router-dom";
+
     
 
 
 
 function App() {
-     Header()
-     HeroSection()
-     Services()
-     Stylists()
-     Location()
-     Footer()
+     
   return (
     <div className="App">
       <Header />
---
+++ client/src/components/landing/heroSection.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+function HeroSection() {
+
+  const navigate = useNavigate;
+
+  function handleSubmit() {
+    navigate("/appointments")
+
+    return (
+      <section className="Hero-section">
+        <h2>Welcome to Salon Don</h2>
+        <p>Your ultimate destination for all hairstyles</p>
+        <p>Welcome to Salon Don, where elegance and expertise converge to offer you a truly exceptional experience. Nestled in a chic and modern setting, our upscale salon provides a comprehensive range of services, from cutting-edge haircuts and color treatments to luxurious spa therapies. Our highly skilled and passionate team is dedicated to delivering personalized attention and top-notch service, using only premium, eco-friendly products. At Salon Don, we pride ourselves on creating a welcoming and tranquil ambiance, ensuring each visit leaves you feeling pampered, rejuvenated, and transformed. Discover the difference at Salon Don, where your satisfaction is our utmost priority.</p>
+        <p>Book your appointment today!</p>
+        <button>Book Now</button>
+      </section>
+      
+    );
+
+  }
+  return (
+    <section className="Hero-section">
+      <h2>Welcome to Salon Don</h2>
+      <p>Your ultimate destination for all hairstyles</p>
+      <button onClick={handleSubmit}>Book Now</button>
+    </section>
+  );
+}
+export default HeroSection;
