427fa53418bbf7c1339bf6feb1c32dcebc392388
Archana Emayavaran <svarchanae@gmail.com>
2024-07-21 21:40:42 -0400

    landing jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,18 @@
 
 import React from 'react';
 import './App.css';
-import Header from './components/LandingPage/headers/headers';
-import HeroSection from './components/LandingPage/heroSection/heroSection';
-import Services from './components/LandingPage/services/services';
-import Stylists from './components/LandingPage/stylist/stylist';
-import Location from './components/LandingPage/location/location';
-import Footer from './components/LandingPage/footer/footer';
+import { Header, HeroSection, Services, Stylists, Location, Footer } from './components/landing/landing.jsx';
+    
 
 
 
 function App() {
+     Header()
+     HeroSection()
+     Services()
+     Stylists()
+     Location()
+     Footer()
   return (
     <div className="App">
       <Header />
--- client/src/components/LandingPage/footer/footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    const currentYear = new Date().getFullYear();
-
-    return(
-        <h6> Salon Don Copyright {currentYear} &copy;</h6>
-    )
-}
-
-export default Footer;
