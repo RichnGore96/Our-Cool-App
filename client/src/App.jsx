17e842a57c89acbfa913b910bfe632726c50ee30
Archana Emayavaran <svarchanae@gmail.com>
2024-07-22 21:24:13 -0400

    folder for landing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-
-
-import React from 'react';
-import './App.css';
-import Header from './components/landing/headers';
-import HeroSection from './components/landing/heroSection';
-import Services from './components/landing/services';
-import Stylists from './components/landing/stylist';
-import Location from './components/landing/location';
-import Footer from './components/landing/footer';
-import { useNavigate } from "react-router-dom";
-
-    
-
-
-=======
 import React from "react";
 import "./App.css";
 import Landing from "./components/LandingPage/Landing";
@@ -26,10 +9,8 @@ import Stylists from './components/LandingPage/stylist/stylist';
 import Location from './components/LandingPage/location/location';
 import Footer from './components/LandingPage/footer/footer';
  */
->>>>>>> c5eb1d94fd2bb22a462d03dec614c0f06f2a399a
 
 function App() {
-     
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
--- client/src/components/LandingPage/Landing.jsx
+++ client/src/components/LandingPage/Landing.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Header from "../LandingPage/headers/headers";
+import Header from "./header/headers";
 import HeroSection from "./heroSection/heroSection";
 import Services from "./services/services";
 import Stylists from "./stylist/stylist";
