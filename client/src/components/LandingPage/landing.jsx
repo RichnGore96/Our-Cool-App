fce2dd397ed70e267d779dcba8e6160a0511d4a1
Archana Emayavaran <svarchanae@gmail.com>
2024-07-24 00:56:56 -0400

    name Landing.jsx

--- client/src/components/LandingPage/landing.jsx
+++ client/src/components/LandingPage/landing.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Header from "../LandingPage/headers/headers";
+import Header from "./headers/headers";
 import HeroSection from "./heroSection/heroSection";
 import Services from "./services/services";
 import Stylists from "./stylist/stylist";
